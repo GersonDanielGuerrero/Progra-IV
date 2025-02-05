cb56da6b1ec321bb45a694f7bf97df1adfab35f5
GersonDanielGuerrero <danicastillog4@gmail.com>
2025-02-05 10:57:32 -0600

    terminado

--- main.js
+++ main.js
@@ -3,22 +3,84 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+            alumnos: [],
+            alumnosMostrados: [],
+            campoFiltrado: 'nombre',
+            filtro: '',
             codigo: '',
             nombre: '',
             direccion: '',
             telefono: '',
-            email: ''
+            email: '',
+            nacimiento: '',
+            sexo: '',
+            departamento: '',
+            municipio: '',
+            distrito: '',
         }
     },
     methods: {
+        eliminarAlumno(alumno) {
+            if (confirm(`¿Esta seguro de eliminar el alumno ${alumno.nombre}?`)){
+                localStorage.removeItem(alumno.codigo);
+                this.listarAlumnos();
+            }
+        },
+        verAlumno(alumno) {
+            this.codigo = alumno.codigo;
+            this.nombre = alumno.nombre;
+            this.direccion = alumno.direccion;
+            this.telefono = alumno.telefono;
+            this.email = alumno.email;
+            this.nacimiento = alumno.nacimiento;
+            this.sexo = alumno.sexo;
+            this.departamento = alumno.departamento;
+            this.municipio = alumno.municipio;
+            this.distrito = alumno.distrito;
+        },
         guardarAlumno() {
-            console.log( 
-                this.codigo,
-                this.nombre,
-                this.direccion,
-                this.telefono,
-                this.email
-            );
+            let alumno = {
+                codigo: this.codigo,
+                nombre: this.nombre,
+                direccion: this.direccion,
+                telefono: this.telefono,
+                email: this.email,
+                nacimiento: this.nacimiento,
+                sexo: this.sexo,
+                departamento: this.departamento,
+                municipio: this.municipio,
+                distrito: this.distrito,
+            };
+            localStorage.setItem(this.codigo, JSON.stringify(alumno));
+            this.listarAlumnos();
+            //limpiar campos
+            this.codigo = '';
+            this.nombre = '';
+            this.direccion = '';
+            this.telefono = '';
+            this.email = '';
+            this.nacimiento = '';
+            this.sexo = '';
+            this.departamento = '';
+            this.municipio = '';
+            this.distrito = '';
+        },
+        listarAlumnos() {
+            this.alumnos = [];
+            for (let i = 0; i < localStorage.length; i++) {
+                let clave = localStorage.key(i),
+                    valor = localStorage.getItem(clave);
+                this.alumnos.push(JSON.parse(valor));
+            }
+            this.alumnosMostrados = this.alumnos;
+        },
+        filtrarAlumnos(){
+            this.alumnosMostrados = this.alumnos.filter(alumno => {
+                return alumno[this.campoFiltrado].toLowerCase().includes(this.filtro.toLowerCase());
+            });
         }
+    },
+    created() {
+        this.listarAlumnos();
     }
 }).mount('#app');
