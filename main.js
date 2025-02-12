55278acbca504ad47edaa10508a2b2172542dac4
GersonDanielGuerrero <danicastillog4@gmail.com>
2025-02-12 11:11:47 -0600

    Clase virtual 3

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const {createApp} = Vue;
+const Dexie = window.Dexie,
+    db = new Dexie('db_academico');
 
 createApp({
     data() {
         return {
+            buscar: '',
+            buscarTipo: 'nombre',
+            accion: 'nuevo',
+            alumnos: [],
+            idAlumno: '',
             codigo: '',
             nombre: '',
             direccion: '',
@@ -11,14 +18,58 @@ createApp({
         }
     },
     methods: {
+        eliminarAlumno(alumno) {
+            if (confirm(`¿Esta seguro de eliminar el alumno ${alumno.nombre}?`)){
+                db.alumnos.delete(alumno.idAlumno);
+                this.listarAlumnos();
+            }
+        },
+        modificarAlumno(alumno) {
+            this.accion = 'modificar';
+            this.idAlumno = alumno.idAlumno;
+            this.codigo = alumno.codigo;
+            this.nombre = alumno.nombre;
+            this.direccion = alumno.direccion;
+            this.telefono = alumno.telefono;
+            this.email = alumno.email;
+        },
         guardarAlumno() {
-            console.log( 
-                this.codigo,
-                this.nombre,
-                this.direccion,
-                this.telefono,
-                this.email
-            );
+            let alumno = {
+                codigo: this.codigo,
+                nombre: this.nombre,
+                direccion: this.direccion,
+                telefono: this.telefono,
+                email: this.email
+            };
+            if (this.accion == 'modificar') {
+                alumno.idAlumno = this.idAlumno;
+            }
+            db.alumnos.put(alumno);
+            this.nuevoAlumno();
+            this.listarAlumnos();
+        },
+        async listarAlumnos() {
+            this.alumnos = await db.alumnos.where(this.buscarTipo).startsWithIgnoreCase(this.buscar).toArray();
+            /*this.alumnos = await db.alumnos.filter(alumno =>{
+                return alumno.nombre.toLowerCase().startsWith(this.buscar.toLowerCase()) || 
+                    alumno.codigo.toLowerCase().startsWith(this.buscar.toLowerCase());
+            }).toArray();*/
+
+        },
+        nuevoAlumno() {
+            this.accion = 'nuevo';
+            this.idAlumno = '';
+            this.codigo = '';
+            this.nombre = '';
+            this.direccion = '';
+            this.telefono = '';
+            this.email = '';
         }
+    },
+    created() {
+        db.version(1).stores({
+            alumnos: '++idAlumno, codigo, nombre, direccion, telefono, email'
+        });
+        this.listarAlumnos();
     }
 }).mount('#app');
