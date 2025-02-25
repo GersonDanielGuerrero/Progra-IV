39c2ac3ef891b1bad211e7b12f985d23b69cd903
GersonDanielGuerrero <danicastillog4@gmail.com>
2025-02-25 14:51:54 -0600

    clase 4

--- main.js
+++ main.js
@@ -1,82 +1,41 @@
-const {createApp} = Vue;
+const {createApp, ref} = Vue;
 const Dexie = window.Dexie,
     db = new Dexie('db_academico');
 
-createApp({
+const app = createApp({
+    components: {
+        alumno,
+        materia,
+        buscaralumno,
+        buscarmateria
+    },
     data() {
         return {
-            buscar: '',
-            buscarTipo: 'nombre',
-            accion: 'nuevo',
-            alumnos: [],
-            idAlumno: '',
-            codigo: '',
-            nombre: '',
-            direccion: '',
-            telefono: '',
-            email: ''
-        }
+            forms : {
+                alumno: {mostrar: false},
+                buscarAlumno: {mostrar: false},
+                materia: {mostrar: false},
+                buscarMateria: {mostrar: false},
+                matricula: {mostrar: false},
+            },
+        };
     },
     methods: {
-        eliminarAlumno(alumno) {
-            if (confirm(`¿Esta seguro de eliminar el alumno ${alumno.nombre}?`)){
-                db.alumnos.delete(alumno.idAlumno);
-                this.listarAlumnos();
-            }
+        buscar(form, metodo) {
+            this.$refs[form][metodo]();
         },
-        modificarAlumno(alumno) {
-            this.accion = 'modificar';
-            this.idAlumno = alumno.idAlumno;
-            this.codigo = alumno.codigo;
-            this.nombre = alumno.nombre;
-            this.direccion = alumno.direccion;
-            this.telefono = alumno.telefono;
-            this.email = alumno.email;
+        abrirFormulario(componente) {
+            this.forms[componente].mostrar = !this.forms[componente].mostrar;
         },
-        guardarAlumno() {
-            let alumno = {
-                codigo: this.codigo,
-                nombre: this.nombre,
-                direccion: this.direccion,
-                telefono: this.telefono,
-                email: this.email
-            };
-            if (this.accion == 'modificar') {
-                alumno.idAlumno = this.idAlumno;
-            }
-            db.alumnos.put(alumno);
-            this.nuevoAlumno();
-            this.listarAlumnos();
-        },
-        async listarAlumnos() {
-            this.alumnos = await db.alumnos.where(this.buscarTipo).startsWithIgnoreCase(this.buscar).toArray();
-            /*this.alumnos = await db.alumnos.filter(alumno =>{
-                return alumno.nombre.toLowerCase().startsWith(this.buscar.toLowerCase()) || 
-                    alumno.codigo.toLowerCase().startsWith(this.buscar.toLowerCase());
-            }).toArray();*/
-
-        },
-        nuevoAlumno() {
-            this.accion = 'nuevo';
-            this.idAlumno = '';
-            this.codigo = '';
-            this.nombre = '';
-            this.direccion = '';
-            this.telefono = '';
-            this.email = '';
+        modificar(form, metodo, datos) {
+            this.$refs[form][metodo](datos);
         }
     },
     created() {
         db.version(1).stores({
-            alumnos: '++idAlumno, codigo, nombre, direccion, telefono, email'
+            alumnos: '++idAlumno, codigo, nombre, direccion, telefono, email',
+            materias: '++idMateria, codigo, nombre, uv',
         });
-        this.listarAlumnos();
     }
-}).mount('#app');
-
-
-//Tarea
-/*
-** Listar cumpleañeros del mes, menores de edad, menores de 21
-
-*/
+});
+app.mount('#app');
