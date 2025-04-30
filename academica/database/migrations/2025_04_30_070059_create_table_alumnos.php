7405413f1ad4a451fd357ff74c1f7fa153992684
GersonDanielGuerrero <danicastillog4@gmail.com>
2025-04-30 01:46:01 -0600

    cositas

---
+++ academica/database/migrations/2025_04_30_070059_create_table_alumnos.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo',10)->unique();
+            $table->string('nombre',75);
+            $table->string('direccion',150);
+            $table->string('telefono',9);
+            $table->string('email',150);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_alumnos');
+    }
+};
--- academica/routes/web.php
+++ academica/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/bienvenida', function () {
+    return 'Bienvenidos a Programacion Computacional IV.';
+});
+Route::get('/usuario/{id}/{nombre}/{apellido}', function ($id, $nombre, $apellido) {
+    return 'User #: '.$id. ', Nombre: '.$nombre .' Apellido: '.$apellido;
+})->where('id', '[0-9]+');
+
+Route::controller(AlumnoController::class)->group(function () {
+    Route::get('/alumno', 'index');
+    Route::post('/alumno', 'store');
+    Route::put('/alumno', 'update');
+    Route::delete('/alumno', 'destroy');
+});
